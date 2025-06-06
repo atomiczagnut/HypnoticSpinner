16420a3096e973b68041bdc87bb4bbb313f69d9d
atomiczagnut <atomiczagnut@gmail.com>
2025-06-06 12:35:21 -0700

    Fixed error importing Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Analytics } from '@vercel/analytics/next';
+import { Analytics } from '@vercel/analytics/react';
 import spinningLogo from './assets/FractoscopicIcon.png'
 import Slider from 'rc-slider'
 import 'rc-slider/assets/index.css'
